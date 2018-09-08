d47d72f1c6117000d5b41e951d51cc39a537499d
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-08 01:51:44 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+  # projects: kickstarter.css("li.project.grid_4")
 
 end
 binding.pry
 create_project_hash
+ 
